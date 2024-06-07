d427867069102a79a638036111e12cfe2163f24f
Lius <morarolivio@gmail.com>
2024-06-07 12:25:45 +0200

    fix potential bugs

--- scooters/app/storage.py
+++ scooters/app/storage.py
@@ -6,9 +6,8 @@ from app import models
 
 async def get_scooters(context: AppContext) -> tp.List[models.Scooter]:
     sql = """
-    select id, location, scooters.user from scooters
+    select scooters.id, scooters.location, scooters.user from scooters
     """
     rows = await context.db.fetch(sql)
-    print(rows)
 
     return [models.Scooter.from_db(row) for row in rows]
